aef817a8638973d823a0b212b6c31439da8ba09b
ivantriyadi <ivantriyadi@gmail.com>
2025-01-13 20:07:44 +0700

    Update 1_Dataset_cleaning.py

--- pages/1_Dataset_cleaning.py
+++ pages/1_Dataset_cleaning.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.subheader("Proses pembersihan dataset")
+st.subheader("Proses Pembersihan Dataset")
 st.write('Raw tweet dalam dataset pandas')
 #st.image("assets/gambarnya.jpg")
 
@@ -21,8 +21,7 @@ elif proses == 'Data Understanding':
     st.markdown(
         """
     Pada proses ini, tweets yang sudah dimasukkan ke dalam dataset pandas diperiksa tipe dan jumlah datanya, periksa apakah
-    ada baris kosong dan duplikat. Untuk isi tweet yang sama, saya tidak anggap sebagai duplikat karena bisa jadi merupakan
-    retweet atau post dari berbeda akun. Dik
+    ada baris kosong dan duplikat. Diketahui terdapat 100 tweets, tidak terdapat baris kosong pada kolom Tweet dan tidak terdapat tweet duplikat.
         """
     )
     st.image("assets/1-understanding.jpg")
@@ -38,7 +37,13 @@ elif proses == 'Text Preprocessing':
     st.image("assets/1-preprocessing.jpg")
 
 elif proses == 'Tokenization':
-    st.subheader('Proses tokenization')
+    st.subheader('Proses tokenization dan stemming')
+    st.markdown(
+        """
+    Pada proses ini, string pada tiap tweet dipecah menjadi token, lalu dicari akar katanya (stemming) menggunakan library Sastrawi agar lebih
+    akurat terhadap tweet yang berbahasa indonesia. Script akan menghasilkan file csv berupa Tweet yang sudah siap untuk dilakukan analisis sentimen.
+        """
+    )
     st.image("assets/1-tokenization.jpg")
     with open('assets/Clean_tweet.csv') as f:
         st.download_button(
