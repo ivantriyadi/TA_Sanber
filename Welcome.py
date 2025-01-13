8cf6fde76026d9d532296e88122d6f3ee9953f6a
ivantriyadi <ivantriyadi@gmail.com>
2025-01-13 17:20:10 +0700

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -23,7 +23,7 @@ Metodologi:
 1.	Pengambilan dataset dilakukan manual melalui chrome.
 2.	Melakukan pembersihan dataset menggunakan library Sastrawi, agar stemming lebih akurat -> pada file Dataset_cleaning.ipynb
 3.	Melakukan terjemahan bahasa Indonesia ke Inggris dalam pengolahan sentimen analysis menggunakan library NLTK -> pada file Dataset_processing.ipynb
-4.	Visualisasi presentase sentimen keberpihakan (positif, negative dan netral), akurasi model, dan cloud of words.
+4.	Visualisasi presentase sentimen perasaan (positif, negative dan netral) dan cloud of words.
 5.	Data, pengolahan dan visualisasi ditampilkan melalui interface Streamlit
 
 Algoritma dan Model:
@@ -37,9 +37,8 @@ Regex untuk proses pembersihan string dari hyperlink, angka, tanda baca dan lain
 Pandas untuk pengolahan dataset.
 Matplotly untuk menampilkan visualisasi data dalam grafik.
 
-Dibuat oleh: Ivan Triyadi\n
-Tugas Akhir Data Science Sanbercampus\n
-Trainer: Thio Perdana\n
+Dibuat oleh: Ivan Triyadi, untuk Tugas Akhir Data Science Sanbercampus\n
+Trainer: Thio Perdana
     """
 )
 
