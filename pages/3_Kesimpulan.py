4f19b1bcf82d6f06b23297d8db3423a04c51801d
ivantriyadi <ivantriyadi@gmail.com>
2025-01-13 21:36:33 +0700

    Update 3_Kesimpulan.py

--- pages/3_Kesimpulan.py
+++ pages/3_Kesimpulan.py
@@ -1,12 +1,26 @@
 import streamlit as st
-import pandas as pd
 
 st.subheader("Kesimpulan")
 
-#st.image("assets/gambarnya.jpg")
+st.markdown(
+    """
+Dari hasil analisis sentimen dan visualisasi diperolah diagram pie-chart sebagai berikut. Terlihat bahwa mayoritas (83%) sample
+tweet bersentimen positif dalam menghadapi tahun 2025. Sebanyak 11% sample bersentimen negatif. Dan 6% sample bersentimen netral.
+Sentimen netral ini hampir semuanya adalah tweet promosi yang memanfaatkan event tahun baru untuk menarik perhatian customer.
+    """
+)
+st.image("assets/2-pie.jpg")
 
 st.markdown(
     """
-Tulis di sini
+Dari hasil analisis sentimen dan visualisasi diperolah wordcloud sentimen negatif sebagai berikut.
     """
 )
+st.image("assets/2-negatif.jpg")
+
+st.markdown(
+    """
+Dari hasil analisis sentimen dan visualisasi diperolah wordcloud sentimen positif sebagai berikut.
+    """
+)
+st.image("assets/2-positif.jpg")
